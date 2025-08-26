ae0245f7be5c2a94070ee834e4948919f47db4ec
Santiago Salamanca <sansalrus@gmail.com>
2025-08-25 23:44:34 -0500

    Primera parte del enrutamiento

--- src/App.vue
+++ src/App.vue
@@ -12,7 +12,8 @@ import HelloWorld from './components/HelloWorld.vue'
 
       <nav>
         <RouterLink to="/">Home</RouterLink>
-        <RouterLink to="/about">About</RouterLink>
+        <RouterLink to="/skills">Skills</RouterLink>
+        <RouterLink to="/badges">Badges</RouterLink>
       </nav>
     </div>
   </header>
